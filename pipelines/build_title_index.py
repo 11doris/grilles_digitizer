594848df4f804bc99587b1ecb6ea25a501dabe95
doris <86537242+11doris@users.noreply.github.com>
2026-07-09 19:02:01 +0200

    Thou Swell: distinguish old version and modern version.

--- pipelines/build_title_index.py
+++ pipelines/build_title_index.py
@@ -64,7 +64,8 @@ MANUAL = [
     ("266_03_MILESTONES.png", "534_01_MILESTONES.png"),
     ("466_01_WILD_CAT_BLUES_PART2.png", "929_01_WILD_CAT_BLUES.png"),
     ("185_01_I_LOVE_YOU.png", "351_01_I_LOVE_YOU.png"),
-]
+    ("425_01_THOU_SWELL_MODERN_VERSION.png", "847_01_THOU_SWELL.png"),
+    ]
 
 
 def main():
